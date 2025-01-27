443e1e41fa07e9d080e98d31b4c7422cab608843
Oliver-K3003 <Oliverkramer3003@gmail.com>
2025-01-26 19:07:07 -0500

    fixing display and added list selection

--- lua/hotplate/init.lua
+++ lua/hotplate/init.lua
@@ -36,8 +36,8 @@ M.addBP = function()
     local name = getName('Enter boilierplate name: \n')
 
     -- gathering starting and end position of selection 
-    local vStart = vim.fn.getpos("'<")
-    local vEnd = vim.fn.getpos("'>")
+    local vStart = vim.fn.getpos(".")
+    local vEnd = vim.fn.getpos("v")
     -- start and end line only
     local startPos = vStart[2]
     local endPos = vEnd[2]
@@ -55,7 +55,22 @@ M.removeBP = function()
 end
 
 M.useBP = function()
-    local name = getName('Enter boilierplate name to use: \n')
+    local name
+    local keys = {}
+    local n = 0
+    for key, val in pairs(BP_Table) do
+        n = n+1
+        keys[n] = key
+    end
+    vim.ui.select(keys, {
+        prompt = 'Select boilierplate to use',
+        format_item = function(item)
+            return(item)
+        end,
+    }, function(choice)
+        name = choice
+    end)
+
     if BP_Table[name] then
         vim.api.nvim_put(BP_Table[name], "", true, true)
     else
@@ -64,7 +79,7 @@ M.useBP = function()
 end
 
 M.listBP = function()
-    print(vim.inspect(BP_Table))
+    vim.notify(vim.inspect(BP_Table), INFO)
 end
 
 M.setup()
